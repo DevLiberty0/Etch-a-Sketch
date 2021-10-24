b57bc5d3547e35286f1dac19b6f998d89e4f2515
Adam <liberty0@wp.pl>
2021-10-24 20:48:02 +0200

    nowa wersja background

--- script.js
+++ script.js
@@ -1,11 +1,23 @@
+const backgroundSquare = document.querySelector('.background-square');
 const container = document.querySelector('#container');
-let box = [];
+const body = document.querySelector('body');
+let box = [], square = [];
 
-container.style.cssText = `display: grid; grid-template-columns: 1fr 1fr 1fr 1fr`;
+for(let i = 0; i < 20; i++) {
+    square[i] = document.createElement('div');
+    square[i].setAttribute('class', 'background-square');
+    let rozmiar = Math.floor(Math.random() * 100) + 10;
+    let posX = Math.floor(Math.random() * window.innerHeight);
+    let posY = Math.floor(Math.random() * window.innerWidth);
+    square[i].style.width = rozmiar + "px";
+    square[i].style.height = rozmiar + "px";
+    square[i].style.marginLeft = posY + "px";
+    square[i].style.marginTop = posX + "px";
+    body.appendChild(square[i]);
+}
 
-
-for(let i = 0; i < 16; i++) {
+for(let i = 0; i < 25; i++) {
     box[i] = document.createElement('div');
-    box[i].style.cssText = 'width: 50px; height: 50px; background-color: black';
+    box[i].setAttribute('id', 'box');
     container.appendChild(box[i]);
 }
